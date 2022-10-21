2e6eb91efdc0945befbdf88bb7bf3d093f6f9224
Liudmila D <verliebtin@mail.ru>
2022-10-21 15:38:05 +0300

    Finish 2

--- file.py
+++ file.py
@@ -1,54 +1,57 @@
 import ui
 
 def readCsv():
-    f1 = open('phoneBook.csv', 'r')
-    lst = f1.readline()
+    file = open('phoneBook.csv', 'r')
+    lst = file.readline()
     phoneBook = []
     while len(lst) > 0:
         record = lst.split(';')
         phoneBook.append(record)
-        lst = f1.readline()
-
+        lst = file.readline()
+    file.close()
     return phoneBook
 
+
 def saveCsv(phoneBook):
-    f2 = open('phoneBook.csv', 'w')
+    file = open('phoneBook.csv', 'w')
     for record in range(len(phoneBook)):
         for item in range(len(phoneBook[record])):
-            f2.write(str(phoneBook[record][item]).strip())
+            file.write(str(phoneBook[record][item]).strip())
             if item != len(phoneBook[record])-1:
-                f2.write(';')
+                file.write(';')
         if record != len(phoneBook)-1:
-            f2.write('\n')
-    f2.close()
+            file.write('\n')
+    file.close()
+
 
 def readTxt():
-    f1 = open('phoneBook.txt', 'r')
+    file = open('phoneBook.txt', 'r')
     phoneBook = []
     while True:
         record = []
-        item = f1.readline()
+        item = file.readline()
         if len(item) > 0:
             item = item.replace('\r', '').replace('\n', '')
             record.append(item)
             for i in range(3):
-                item = f1.readline()
+                item = file.readline()
                 item = item.replace('\r', '').replace('\n', '')
                 record.append(item)
             phoneBook.append(record)
-            item = f1.readline()
+            item = file.readline()
             if len(item) == 0:
                 break
-
+    file.close()
     return phoneBook
 
+
 def saveTxt(phoneBook):
-    f2 = open('phoneBook.txt', 'w')
+    file = open('phoneBook.txt', 'w')
     for record in range(len(phoneBook)):
         for item in range(len(phoneBook[record])):
-            f2.write(str(phoneBook[record][item]).strip())
+            file.write(str(phoneBook[record][item]).strip())
             if item != len(phoneBook[record])-1:
-                f2.write('\n')
+                file.write('\n')
         if item == len(phoneBook[record])-1 and record != len(phoneBook)-1:
-            f2.write('\n\n')
-    f2.close()
+            file.write('\n\n')
+    file.close()
